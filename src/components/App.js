256d91751857701a82e704a352cdb236c7c6acaf
Shiwani <96593318+Shiwani-325@users.noreply.github.com>
2022-10-10 17:31:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import '../styles/App.css';
 import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [name , setName] = useState('');
+  const [id ,setId] = useState(1)
 //code here 
- const[name, setName]=useState(");
-                 const[id,setId]=useState(1);    
-                                          useEffect(()=>{
- fetch('https://content.newtonschool.co/v1/pr/main/users/${id}')
- .then(response => response.json())
- .then((data) =>{ 
-  console.log(data.name);
-  setName(data.name)
- })
- },[id])
- const changeInput=(e)=>{
+useEffect(()=>{
+  fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`).then((resp)=>resp.json()).then((data)=>{
+    console.log(data.name);
+    setName(data.name)
+  })
+} ,[id])
+const changeInput=(e)=>{
   setId(e.target.value)
- }
+}
+ 
 
 
 
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" onChange={(e)=>changeInput(e)} />
       <p id="name">{name}</p>
     </div>
   );
